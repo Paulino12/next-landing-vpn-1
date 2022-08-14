6428a0f1c5c9eefa5dcc582691fc156ed842efe5
Lino <Pchuitcheu@yahoo.fr>
2022-08-14 14:34:07 +0100

    image

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
+import Image from 'next/image'
 // Import react scroll
 import { Link as LinkScroll } from "react-scroll";
 import ButtonOutline from "../misc/ButtonOutline.";
-import LogoVPN from "../../public/assets/logo.svg";
 
 const Header = () => {
   const [activeLink, setActiveLink] = useState(null);
@@ -23,7 +23,7 @@ const Header = () => {
       >
         <nav className="max-w-screen-xl px-6 sm:px-8 lg:px-16 mx-auto grid grid-flow-col py-3 sm:py-4">
           <div className="col-start-1 col-end-2 flex items-center">
-            <LogoVPN className="h-6 md:h-12 w-auto" />
+            <Image src="/assets/logo.svg" layout="intrinsic" width="150" height="50" alt="logo" />
           </div>
           <ul className="hidden lg:flex col-start-4 col-end-8 text-black-500  items-center">
             <LinkScroll
